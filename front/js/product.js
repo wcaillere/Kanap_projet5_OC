b0407da866c3d9288ffc3d1a96dae38718659271
William Caillere <wcaillere.pro@gmail.com>
2022-08-17 10:27:27 +0200

    Ajout du reste des données API à la page product

--- front/js/product.js
+++ front/js/product.js
@@ -9,7 +9,13 @@ fetch(`http://localhost:3000/api/products/${id}`)
     })
     .then(function(product) {
         console.log(product);
-        document.querySelector('.item__img').innerHTML = `<img src=${product.imageUrl} alt=${product.altTxt}>`
+        document.querySelector('.item__img').innerHTML = `<img src=${product.imageUrl} alt=${product.altTxt}>`;
+        document.querySelector('#title').textContent = product.name;
+        document.querySelector('#price').textContent = product.price;
+        document.querySelector('#description').textContent = product.description;
+        for (let color of product.colors) {
+            document.querySelector('#colors').innerHTML += `<option value=${color.toLowerCase()}>${color.toLowerCase()}</option>`;
+        }
     })
     .catch(function(err) {
         console.log(err);

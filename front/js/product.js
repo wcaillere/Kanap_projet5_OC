fd7d2874fc037393e7c78932290583c4d9a006f0
William Caillere <wcaillere.pro@gmail.com>
2022-08-17 10:04:11 +0200

    Récupération du produit par l'API pour la page product.html

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,15 @@
 let url = new URL(window.location.href);
 let id = url.searchParams.get('id');
 
-console.log(id);
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then(function(data) {
+        if (data.ok) {
+            return data.json();
+        }
+    })
+    .then(function(product) {
+        console.log(product);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });

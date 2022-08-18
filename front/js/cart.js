a4b88ff997914b48dc632352c6a1db67561b955d
William Caillere <wcaillere.pro@gmail.com>
2022-08-18 13:58:00 +0200

    ajout affichage panier dans une fonction

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,39 +2,44 @@ let cartList = JSON.parse(localStorage.getItem('cart'));
 
 // console.log(cartList);
 
-for (let item of cartList) {
-    fetch(`http://localhost:3000/api/products/${item.id}`)
-    .then(function(data) {
-        if (data.ok) {
-            return data.json();
-        }
-    })
-    .then(function(product) {
+/**
+ * Show the cart on the cart page
+ */
+function showCart () {
+  for (let item of cartList) {
+      fetch(`http://localhost:3000/api/products/${item.id}`)
+      .then(function(data) {
+          if (data.ok) {
+              return data.json();
+          }
+      })
+      .then(function(product) {
         document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-        <div class="cart__item__img">
-          <img src="${product.imageUrl}" alt="${product.altTxt}">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>${product.name}</h2>
-            <p>${item.color}</p>
-            <p>${product.price} €</p>
+          <div class="cart__item__img">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
           </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${product.name}</h2>
+              <p>${item.color}</p>
+              <p>${product.price} €</p>
             </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+              </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+              </div>
             </div>
           </div>
-        </div>
-      </article>`
-    })
-    .catch(function(err) {
-        console.log(err);
-    }); 
+        </article>`
+      })
+      .catch(function(err) {
+          console.log(err);
+      }); 
+  }
+};
 
-
-}
+showCart();

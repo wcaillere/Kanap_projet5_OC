bfca7c475bd90130f0971b262e7d03add6f17dee
William Caillere <wcaillere.pro@gmail.com>
2022-08-18 16:31:38 +0200

    Ajout fonction removeFromCart + changement place fonctions dans le code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,3 @@
-let cartList = JSON.parse(localStorage.getItem('cart'));
-
-// console.log(cartList);
-
 /**
  * Save the new cart in the localStorage
  * @param {Array} cart 
@@ -22,46 +18,6 @@ function getCart() {
       return JSON.parse(cart);
   }
 }
-
-/**
- * Show the cart on the cart page
- */
-function showCart () {
-  for (let item of cartList) {
-      fetch(`http://localhost:3000/api/products/${item.id}`)
-      .then(function(data) {
-          if (data.ok) {
-              return data.json();
-          }
-      })
-      .then(function(product) {
-        document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
-          <div class="cart__item__img">
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-          </div>
-          <div class="cart__item__content">
-            <div class="cart__item__content__description">
-              <h2>${product.name}</h2>
-              <p>${item.color}</p>
-              <p>${product.price} €</p>
-            </div>
-            <div class="cart__item__content__settings">
-              <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
-              </div>
-              <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-              </div>
-            </div>
-          </div>
-        </article>`
-      })
-      .catch(function(err) {
-          console.log(err);
-      }); 
-  }
-};
 
 function getTotalQuantity() {
   let cart = getCart();
@@ -92,6 +48,60 @@ function getTotalPrice() {
   }
 }
 
+function removeFromCart(element) {
+  elementToRemove = element.target.closest('.cart__item')
+  let cart = getCart()
+  cart = cart.filter(p => p.id != elementToRemove.dataset.id || p.color != elementToRemove.dataset.color);
+  saveCart(cart);
+  elementToRemove.remove();
+  getTotalPrice();
+  getTotalQuantity();
+}
+
+/**
+ * Show the cart on the cart page
+ */
+function showCart () {
+  let cartList = JSON.parse(localStorage.getItem('cart'));
+  for (let item of cartList) {
+      fetch(`http://localhost:3000/api/products/${item.id}`)
+      .then(function(data) {
+          if (data.ok) {
+              return data.json();
+          }
+      })
+      .then(function(product) {
+        document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${item.id}" data-color="${item.color}">
+          <div class="cart__item__img">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+          </div>
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${product.name}</h2>
+              <p>${item.color}</p>
+              <p>${product.price} €</p>
+            </div>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+              </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+              </div>
+            </div>
+          </div>
+        </article>`
+        document.querySelector('.deleteItem').addEventListener('click', (e) => {
+          removeFromCart(e);
+        })
+      })
+      .catch(function(err) {
+          console.log(err);
+      }); 
+  }
+  getTotalQuantity();
+  getTotalPrice();
+};
+
 showCart();
-getTotalQuantity();
-getTotalPrice();

865a1997a300412f260e2ef5485c42c207b38d3b
William Caillere <wcaillere.pro@gmail.com>
2022-08-31 12:11:45 +0200

    Suppression commentaire inutile

--- front/js/script.js
+++ front/js/script.js
@@ -34,14 +34,6 @@ function displayProducts() {
             sofaDescription.textContent = product.description;
             sofaDescription.classList.add('productDescription')
             sofaArticle.appendChild(sofaDescription);
-
-        //     document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product._id}">
-        //     <article>
-        //       <img src=${product.imageUrl} alt=${product.altTxt}>
-        //       <h3 class="productName">${product.name}</h3>
-        //       <p class="productDescription">${product.description}</p>
-        //     </article>
-        //   </a>`;
         }
     })
     .catch(function(err) {
